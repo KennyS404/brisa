53037892c27583645e430ff1143c9c7024e30769
Kennys404 <kennyldo48@gmail.com>
2024-08-08 15:38:42 -0300

    feat: multi thread added

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,27 +1,97 @@
-import express, { Request, Response } from "express";
+import express, { raw, Request, Response } from "express";
 import axios from "axios";
 import bodyParser from "body-parser";
 import mongoose from "mongoose";
 import Question from "./models/question";
 import { contextText } from "./context";
+import cluster from "cluster";
+import os from "os";
 
-const app = express();
 const port = 5000;
-
 const mongoPassword = "ytRdoukyZBXsYHv1";
 
-app.use(bodyParser.json());
-
-mongoose
-  .connect(`mongodb://admin:${mongoPassword}@localhost:27017/questions`, {
-    authSource: "admin",
-  })
-  .then(() => {
+// Função para iniciar o servidor Express
+const startServer = async () => {
+  try {
+    await mongoose.connect(
+      `mongodb://admin:${mongoPassword}@localhost:27017/questions`,
+      {
+        authSource: "admin",
+      }
+    );
     console.log("Connected to MongoDB");
-  })
-  .catch((err) => {
+
+    const app = express();
+    app.use(bodyParser.json());
+
+    app.post("/generate", async (req: Request, res: Response) => {
+      const { prompt, senderId, pushName, messageTimestamp } = req.body;
+
+      if (!prompt || !senderId || !pushName || !messageTimestamp) {
+        return res.status(400).json({
+          error:
+            "Prompt, senderId, pushName e messageTimestamp são obrigatórios",
+        });
+      }
+
+      try {
+        const newQuestion = new Question({
+          prompt,
+          senderId,
+          pushName,
+          messageTimestamp,
+        });
+        await newQuestion.save();
+
+        const context = await getContext(senderId);
+        // console.log(prompt, context);
+        const url = "http://localhost:11434/api/generate";
+        const apiData = {
+          model: "llama3",
+          prompt: `Você é um assistente virtual da Fflip e sempre 
+          responde de forma educada e profissional e resumida. Sempre * CITE 
+          O NOME DO USUÁRIO NA MENSAGEM O NOME DO USUÁRIO É ${pushName}* #IMPORTANTE *Se a 
+          pergunta não estiver relacionada ao CONTEXTO DA EMPRESA, diga que só pode responder a 
+          perguntas relacionadas ao suporte técnico da Fflip Solutions de forma educada, resumida
+           e profissional*.\nESSE É O CONTEXTO DA EMPRESA: ${contextText}\nMensagens anteriores:\n${context}\n\nUsuário:
+            ${pushName}: ${prompt} #Não esqueça de citar o nome do usuário na sua resposta, pois você é um
+             assistente virtual e responde de forma humanizada`,
+          stream: false,
+          raw: true,
+        };
+        console.log(url);
+
+        const apiResponse = await axios.post(url, apiData);
+        console.log(apiResponse.data);
+        const message =
+          apiResponse.data.response || "Nenhuma resposta encontrada";
+
+        newQuestion.response = message;
+        await newQuestion.save();
+
+        res.status(200).json({ message });
+      } catch (error) {
+        console.error("Erro:", error);
+
+        if (axios.isAxiosError(error)) {
+          res.status(error.response?.status || 500).json({
+            error: `Falha ao obter resposta. Código de status: ${
+              error.response?.status || "desconhecido"
+            }`,
+          });
+        } else {
+          res.status(500).json({ error: "Ocorreu um erro inesperado" });
+        }
+      }
+    });
+
+    app.listen(port, "0.0.0.0", () => {
+      console.log(`Servidor está rodando em http://0.0.0.0:${port}`);
+    });
+  } catch (err) {
     console.error("Error connecting to MongoDB", err);
-  });
+  }
+};
 
 const getContext = async (senderId: string): Promise<string> => {
   const questions = await Question.find({ senderId })
@@ -32,62 +102,18 @@ const getContext = async (senderId: string): Promise<string> => {
     .join("\n");
 };
 
-app.post("/generate", async (req: Request, res: Response) => {
-  const { prompt, senderId, pushName, messageTimestamp } = req.body;
-
-  if (!prompt || !senderId || !pushName || !messageTimestamp) {
-    return res.status(400).json({
-      error: "Prompt, senderId, pushName e messageTimestamp são obrigatórios",
-    });
+if (cluster.isMaster) {
+  const numCPUs = os.cpus().length;
+  for (let i = 0; i < numCPUs; i++) {
+    cluster.fork();
   }
 
-  const newQuestion = new Question({
-    prompt,
-    senderId,
-    pushName,
-    messageTimestamp,
+  cluster.on("exit", (worker, code, signal) => {
+    console.log(`Worker ${worker.process.pid} died`);
   });
-  await newQuestion.save();
-
-  const context = await getContext(senderId);
-  const url = "http://localhost:11434/api/generate";
-  const apiData = {
-    model: "llama3",
-    prompt: `Você é um assistente virtual da Fflip e sempre 
-    responde de forma educada e profissional e resumida. Sempre * CITE 
-    O NOME DO USUÁRIO NA MENSAGEM O NOME DO USUÁRIO É ${pushName}* #IMPORTANTE *Se a 
-    pergunta não estiver relacionada ao CONTEXTO DA EMPRESA, diga que só pode responder a 
-    perguntas relacionadas ao suporte técnico da Fflip Solutions de forma educada, resumida
-     e profissional*.\nESSE É O CONTEXTO DA EMPRESA: ${contextText}\nMensagens anteriores:\n${context}\n\nUsuário:
-      ${pushName}: ${prompt} #Não esqueça de citar o nome do usuário na sua resposta, pois você é um
-       assistente virtual e responde de forma humanizada`,
-    stream: false,
-  };
-
-  try {
-    const apiResponse = await axios.post(url, apiData);
-
-    const message = apiResponse.data.response || "Nenhuma resposta encontrada";
-
-    newQuestion.response = message;
-    await newQuestion.save();
-
-    res.status(200).json({ message });
-  } catch (error) {
-    console.error("Erro:", error);
-
-    if (axios.isAxiosError(error)) {
-      res.status(error.response?.status || 500).json({
-        error: `Falha ao obter resposta. Código de status: ${
-          error.response?.status || "desconhecido"
-        }`,
-      });
-    } else {
-      res.status(500).json({ error: "Ocorreu um erro inesperado" });
-    }
+} else {
+  // Apenas um worker deve iniciar o servidor e conectar ao MongoDB
+  if (cluster.worker?.id === 1) {
+    startServer();
   }
-});
-
-app.listen(port, "0.0.0.0", () => {
-  console.log(`Servidor está rodando em http://0.0.0.0:${port}`);
-});
+}
--- whatsapp/src/index.ts
+++ whatsapp/src/index.ts
@@ -1,46 +1,115 @@
 // Importando os módulos necessários
-import { Boom } from '@hapi/boom';
-import NodeCache from 'node-cache';
-import readline from 'readline';
-import { AnyMessageContent, delay, DisconnectReason, fetchLatestBaileysVersion, getAggregateVotesInPollMessage, makeCacheableSignalKeyStore, makeInMemoryStore, PHONENUMBER_MCC, useMultiFileAuthState, WAMessageContent, WAMessageKey } from '@whiskeysockets/baileys';
-import MAIN_LOGGER from './logger';
-import proto from '@whiskeysockets/baileys';
-import open from 'open';
-import fs from 'fs';
-import { makeWASocket } from '@whiskeysockets/baileys';
-import axios from 'axios';
+import { Boom } from "@hapi/boom";
+import NodeCache from "node-cache";
+import readline from "readline";
+import {
+  AnyMessageContent,
+  delay,
+  DisconnectReason,
+  fetchLatestBaileysVersion,
+  getAggregateVotesInPollMessage,
+  makeCacheableSignalKeyStore,
+  makeInMemoryStore,
+  PHONENUMBER_MCC,
+  useMultiFileAuthState,
+  WAMessageContent,
+  WAMessageKey,
+} from "@whiskeysockets/baileys";
+import MAIN_LOGGER from "./logger";
+import proto from "@whiskeysockets/baileys";
+import open from "open";
+import fs from "fs";
+import { makeWASocket } from "@whiskeysockets/baileys";
+import axios from "axios";
+import cluster from "cluster";
+import os from "os";
 
 const logger = MAIN_LOGGER.child({});
-logger.level = 'trace';
+logger.level = "trace";
 
-const useStore = !process.argv.includes('--no-store');
-const doReplies = !process.argv.includes('--no-reply');
-const usePairingCode = process.argv.includes('--use-pairing-code');
-const useMobile = process.argv.includes('--mobile');
+const useStore = !process.argv.includes("--no-store");
+const doReplies = !process.argv.includes("--no-reply");
+const usePairingCode = process.argv.includes("--use-pairing-code");
+const useMobile = process.argv.includes("--mobile");
 
 // external map to store retry counts of messages when decryption/encryption fails
 // keep this out of the socket itself, so as to prevent a message decryption/encryption loop across socket restarts
 const msgRetryCounterCache = new NodeCache();
 
 // Read line interface
-const rl = readline.createInterface({ input: process.stdin, output: process.stdout });
-const question = (text: string) => new Promise<string>((resolve) => rl.question(text, resolve));
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+const question = (text: string) =>
+  new Promise<string>((resolve) => rl.question(text, resolve));
 
 // the store maintains the data of the WA connection in memory
 // can be written out to a file & read from it
 const store = useStore ? makeInMemoryStore({ logger }) : undefined;
-store?.readFromFile('./baileys_store_multi.json');
+store?.readFromFile("./baileys_store_multi.json");
 // save every 10s
 setInterval(() => {
-  store?.writeToFile('./baileys_store_multi.json');
+  store?.writeToFile("./baileys_store_multi.json");
 }, 10_000);
+
+// Função para lidar com múltiplas mensagens
+// Função para lidar com múltiplas mensagens
+const handleIncomingMessages = async (messages: Array<any>, sock: any) => {
+  // Cria uma lista de promessas para processar cada mensagem
+  const messagePromises = messages.map(async (msg) => {
+    const {
+      message: { conversation: prompt },
+      key: { remoteJid: senderId },
+      pushName,
+      messageTimestamp,
+    } = msg;
+
+    if (!prompt || !senderId || !pushName || !messageTimestamp) {
+      console.error("Dados incompletos na mensagem:", msg);
+      return;
+    }
+
+    try {
+      const response = await axios.post("http://localhost:5000/generate", {
+        prompt,
+        senderId,
+        pushName,
+        messageTimestamp,
+      });
+      const message = response.data.message;
+      await sendMessageWTyping(sock, { text: message }, senderId!);
+    } catch (error) {
+      console.error("Failed to send message:", error);
+    }
+  });
+
+  // Aguarda todas as promessas serem resolvidas
+  await Promise.all(messagePromises);
+};
+
+const sendMessageWTyping = async (
+  sock: any,
+  msg: AnyMessageContent,
+  jid: string
+) => {
+  await sock.presenceSubscribe(jid);
+  await delay(500);
+
+  await sock.sendPresenceUpdate("composing", jid);
+  await delay(2000);
+
+  await sock.sendPresenceUpdate("paused", jid);
+
+  await sock.sendMessage(jid, msg);
+};
 
 // start a connection
 const startSock = async () => {
-  const { state, saveCreds } = await useMultiFileAuthState('baileys_auth_info');
+  const { state, saveCreds } = await useMultiFileAuthState("baileys_auth_info");
   // fetch latest version of WA Web
   const { version, isLatest } = await fetchLatestBaileysVersion();
-  console.log(`using WA v${version.join('.')}, isLatest: ${isLatest}`);
+  console.log(`using WA v${version.join(".")}, isLatest: ${isLatest}`);
 
   const sock = makeWASocket({
     version,
@@ -66,10 +135,12 @@ const startSock = async () => {
   // Pairing code for Web clients
   if (usePairingCode && !sock.authState.creds.registered) {
     if (useMobile) {
-      throw new Error('Cannot use pairing code with mobile api');
+      throw new Error("Cannot use pairing code with mobile api");
     }
 
-    const phoneNumber = await question('Please enter your mobile phone number:\n');
+    const phoneNumber = await question(
+      "Please enter your mobile phone number:\n"
+    );
     const code = await sock.requestPairingCode(phoneNumber);
     console.log(`Pairing code: ${code}`);
   }
@@ -79,54 +150,75 @@ const startSock = async () => {
     const { registration } = sock.authState.creds || { registration: {} };
 
     if (!registration.phoneNumber) {
-      registration.phoneNumber = await question('Please enter your mobile phone number:\n');
+      registration.phoneNumber = await question(
+        "Please enter your mobile phone number:\n"
+      );
     }
 
-    const libPhonenumber = await import('libphonenumber-js');
-    const phoneNumber = libPhonenumber.parsePhoneNumber(registration!.phoneNumber);
+    const libPhonenumber = await import("libphonenumber-js");
+    const phoneNumber = libPhonenumber.parsePhoneNumber(
+      registration!.phoneNumber
+    );
     if (!phoneNumber?.isValid()) {
-      throw new Error('Invalid phone number: ' + registration!.phoneNumber);
+      throw new Error("Invalid phone number: " + registration!.phoneNumber);
     }
 
-    registration.phoneNumber = phoneNumber.format('E.164');
+    registration.phoneNumber = phoneNumber.format("E.164");
     registration.phoneNumberCountryCode = phoneNumber.countryCallingCode;
     registration.phoneNumberNationalNumber = phoneNumber.nationalNumber;
-    const mcc = PHONENUMBER_MCC[phoneNumber.countryCallingCode as keyof typeof PHONENUMBER_MCC];
+    const mcc =
+      PHONENUMBER_MCC[
+        phoneNumber.countryCallingCode as keyof typeof PHONENUMBER_MCC
+      ];
     if (!mcc) {
-      throw new Error('Could not find MCC for phone number: ' + registration!.phoneNumber + '\nPlease specify the MCC manually.');
+      throw new Error(
+        "Could not find MCC for phone number: " +
+          registration!.phoneNumber +
+          "\nPlease specify the MCC manually."
+      );
     }
 
     registration.phoneNumberMobileCountryCode = mcc as unknown as string;
 
     async function enterCode() {
       try {
-        const code = await question('Please enter the one time code:\n');
-        const response = await sock.register(code.replace(/["']/g, '').trim().toLowerCase());
-        console.log('Successfully registered your phone number.');
+        const code = await question("Please enter the one time code:\n");
+        const response = await sock.register(
+          code.replace(/["']/g, "").trim().toLowerCase()
+        );
+        console.log("Successfully registered your phone number.");
         console.log(response);
         rl.close();
       } catch (error) {
-        console.error('Failed to register your phone number. Please try again.\n', error);
+        console.error(
+          "Failed to register your phone number. Please try again.\n",
+          error
+        );
         await askForOTP();
       }
     }
 
     async function enterCaptcha() {
-      const response = await sock.requestRegistrationCode({ ...registration, method: 'captcha' });
-      const path = __dirname + '/captcha.png';
-      fs.writeFileSync(path, Buffer.from(response.image_blob!, 'base64'));
+      const response = await sock.requestRegistrationCode({
+        ...registration,
+        method: "captcha",
+      });
+      const path = __dirname + "/captcha.png";
+      fs.writeFileSync(path, Buffer.from(response.image_blob!, "base64"));
 
       open(path);
-      const code = await question('Please enter the captcha code:\n');
+      const code = await question("Please enter the captcha code:\n");
       fs.unlinkSync(path);
-      registration.captcha = code.replace(/["']/g, '').trim().toLowerCase();
+      registration.captcha = code.replace(/["']/g, "").trim().toLowerCase();
     }
 
     async function askForOTP() {
       if (!registration.method) {
-        let code = await question('How would you like to receive the one time code for registration? "sms" or "voice"\n');
-        code = code.replace(/["']/g, '').trim().toLowerCase();
-        if (code !== 'sms' && code !== 'voice') {
+        let code = await question(
+          'How would you like to receive the one time code for registration? "sms" or "voice"\n'
+        );
+        code = code.replace(/["']/g, "").trim().toLowerCase();
+        if (code !== "sms" && code !== "voice") {
           return await askForOTP();
         }
 
@@ -137,9 +229,12 @@ const startSock = async () => {
         await sock.requestRegistrationCode(registration);
         await enterCode();
       } catch (error: any) {
-        console.error('Failed to request registration code. Please try again.\n', error);
+        console.error(
+          "Failed to request registration code. Please try again.\n",
+          error
+        );
 
-        if (error?.reason === 'code_checkpoint') {
+        if (error?.reason === "code_checkpoint") {
           await enterCaptcha();
         }
 
@@ -150,33 +245,6 @@ const startSock = async () => {
     askForOTP();
   }
 
-  const sendMessageWTyping = async (msg: AnyMessageContent, jid: string) => {
-    await sock.presenceSubscribe(jid);
-    await delay(500);
-
-    await sock.sendPresenceUpdate('composing', jid);
-    await delay(2000);
-
-    await sock.sendPresenceUpdate('paused', jid);
-
-    await sock.sendMessage(jid, msg);
-  };
-
-  const handleIncomingMessage = async (msg: string, senderId: string, pushName: string, messageTimestamp: number) => {
-    try {
-      const response = await axios.post('http://localhost:5000/generate', {
-        prompt: msg,
-        senderId,
-        pushName,
-        messageTimestamp
-      });
-      const message = response.data.message;
-      await sendMessageWTyping({ text: message }, senderId);
-    } catch (error) {
-      console.error('Failed to send message:', error);
-    }
-  };
-
   // the process function lets you process all events that just occurred
   // efficiently in a batch
   sock.ev.process(
@@ -184,15 +252,18 @@ const startSock = async () => {
     async (events: any) => {
       // something about the connection changed
       // maybe it closed, or we received all offline message or connection opened
-      if (events['connection.update']) {
-        const update = events['connection.update'];
+      if (events["connection.update"]) {
+        const update = events["connection.update"];
         const { connection, lastDisconnect } = update;
-        if (connection === 'close') {
+        if (connection === "close") {
           // reconnect if not logged out
-          if ((lastDisconnect?.error as Boom)?.output?.statusCode !== DisconnectReason.loggedOut) {
+          if (
+            (lastDisconnect?.error as Boom)?.output?.statusCode !==
+            DisconnectReason.loggedOut
+          ) {
             startSock();
           } else {
-            console.log('Connection closed. You are logged out.');
+            console.log("Connection closed. You are logged out.");
           }
         }
 
@@ -200,15 +271,15 @@ const startSock = async () => {
       }
 
       // credentials updated -- save them
-      if (events['creds.update']) {
+      if (events["creds.update"]) {
         await saveCreds();
       }
 
-      if (events['labels.association']) {
+      if (events["labels.association"]) {
         // console.log(events['labels.association'])
       }
 
-      if (events['labels.edit']) {
+      if (events["labels.edit"]) {
         // console.log(events['labels.edit'])
       }
 
@@ -217,42 +288,35 @@ const startSock = async () => {
       }
 
       // history received
-      if (events['messaging-history.set']) {
-        const { chats, contacts, messages, isLatest } = events['messaging-history.set'];
+      if (events["messaging-history.set"]) {
+        const { chats, contacts, messages, isLatest } =
+          events["messaging-history.set"];
         // console.log(`recv ${chats.length} chats, ${contacts.length} contacts, ${messages.length} msgs (is latest: ${isLatest})`)
       }
 
       // received a new message
-      if (events['messages.upsert']) {
-        const upsert = events['messages.upsert'];
-        console.log('recv messages ', JSON.stringify(upsert, undefined, 2));
+      if (events["messages.upsert"]) {
+        const upsert = events["messages.upsert"];
+        console.log("recv messages ", JSON.stringify(upsert, undefined, 2));
 
-        if (upsert.type === 'notify') {
-          for (const msg of upsert.messages) {
-            if (!msg.key.fromMe && doReplies) {
-              console.log('replying to', msg.message.conversation);
-              await sock!.readMessages([msg.key]);
-
-              const pushName = msg.pushName || 'User';
-              const messageTimestamp = msg.messageTimestamp;
-              await handleIncomingMessage(msg.message.conversation, msg.key.remoteJid!, pushName, messageTimestamp);
-            }
-          }
+        if (upsert.type === "notify") {
+          // Usa a função para lidar com todas as mensagens de uma vez
+          await handleIncomingMessages(upsert.messages, sock);
         }
       }
 
       // messages updated like status delivered, message deleted etc.
-      if (events['messages.update']) {
+      if (events["messages.update"]) {
         // console.log(
         // 	JSON.stringify(events['messages.update'], undefined, 2)
         // )
 
-        for (const { key, update } of events['messages.update']) {
+        for (const { key, update } of events["messages.update"]) {
           if (update.pollUpdates) {
             const pollCreation = await getMessage(key);
             if (pollCreation) {
               console.log(
-                'got poll update, aggregation: ',
+                "got poll update, aggregation: ",
                 getAggregateVotesInPollMessage({
                   message: pollCreation,
                   pollUpdates: update.pollUpdates,
@@ -263,40 +327,47 @@ const startSock = async () => {
         }
       }
 
-      if (events['message-receipt.update']) {
+      if (events["message-receipt.update"]) {
         // console.log(events['message-receipt.update'])
       }
 
-      if (events['messages.reaction']) {
+      if (events["messages.reaction"]) {
         // console.log(events['messages.reaction'])
       }
 
-      if (events['presence.update']) {
+      if (events["presence.update"]) {
         // console.log(events['presence.update'])
       }
 
-      if (events['chats.update']) {
+      if (events["chats.update"]) {
         // console.log(events['chats.update'])
       }
 
-      if (events['contacts.update']) {
-        for (const contact of events['contacts.update']) {
-          if (typeof contact.imgUrl !== 'undefined') {
-            const newUrl = contact.imgUrl === null ? null : await sock!.profilePictureUrl(contact.id!).catch(() => null);
-            console.log(`contact ${contact.id} has a new profile pic: ${newUrl}`);
+      if (events["contacts.update"]) {
+        for (const contact of events["contacts.update"]) {
+          if (typeof contact.imgUrl !== "undefined") {
+            const newUrl =
+              contact.imgUrl === null
+                ? null
+                : await sock!.profilePictureUrl(contact.id!).catch(() => null);
+            console.log(
+              `contact ${contact.id} has a new profile pic: ${newUrl}`
+            );
           }
         }
       }
 
-      if (events['chats.delete']) {
-        console.log('chats deleted ', events['chats.delete']);
+      if (events["chats.delete"]) {
+        console.log("chats deleted ", events["chats.delete"]);
       }
     }
   );
 
   return sock;
 
-  async function getMessage(key: WAMessageKey): Promise<WAMessageContent | undefined> {
+  async function getMessage(
+    key: WAMessageKey
+  ): Promise<WAMessageContent | undefined> {
     if (store) {
       const msg = await store.loadMessage(key.remoteJid!, key.id!);
       return msg?.message || undefined;
@@ -307,4 +378,18 @@ const startSock = async () => {
   }
 };
 
-startSock();
+if (cluster.isMaster) {
+  const numCPUs = os.cpus().length;
+  for (let i = 0; i < numCPUs; i++) {
+    cluster.fork();
+  }
+
+  cluster.on("exit", (worker, code, signal) => {
+    console.log(`worker ${worker.process.pid} died`);
+  });
+} else {
+  // Apenas um worker deve iniciar a conexão com o WhatsApp
+  if (cluster.worker.id === 1) {
+    startSock();
+  }
+}
